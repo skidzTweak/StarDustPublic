8ff5b1e4b54c49c517a1eb83c9bf78dac04c28a5
Skidz Tweak <skidz.tweak@gmail.com>
2011-04-23 15:22:03 -0500

    Update of the login page. Thanks Enrico

--- www/sites/main/login.php
+++ www/sites/main/login.php
@@ -1,7 +1,10 @@
-<div id="content"><h2><?= SYSNAME ?>: <? echo $webui_login ?></h2>
+<div id="content">
+    <div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
+    <div id="ContentHeaderCenter"></div>
+    <div id="ContentHeaderRight"><h5><? echo $webui_login; ?></h5></div>
     <div id="login">
-	
-<?if ($_GET[next_page]){
+
+	<?if ($_GET[next_page]){
 	if ($_GET[purchase_id]){?>
 	<form action="index.php?page=<?=$_GET[next_page]?>&purchase_id=<?=$_GET[purchase_id]?>" method="POST" onsubmit="if (!validate(this)) return false;">
 	<?}else{?>
@@ -9,19 +12,25 @@
 <?}}else{?>
 	<form action="index.php" method="POST" onsubmit="if (!validate(this)) return false;">
 <?}?>
-            <table>
-				<tr><td class="error" colspan="2" align="center" id="error_message"><?=$_SESSION[ERROR];$_SESSION[ERROR]="";?><?=$_GET[ERROR]?></td></tr>
+        <table>
+				        <tr>
+                    <td class="error" colspan="2" align="center" id="error_message"><?=$_SESSION[ERROR];$_SESSION[ERROR]="";?><?=$_GET[ERROR]?></td>
+                </tr>
                 <tr>
-                    <td class="odd"><span id="logname_label"><? echo $webui_user_name ?>*</span></td>
-                    <td class="odd"><input require="true" label="logname_label" id="login_input" name="logname" type="text" value="<?= $_POST[logname] ?>" /></td>
+                    <td class="odd" width="51%"><span id="logname_label"><? echo $webui_user_name ?>*</span></td>
+                    <td class="odd"><div class="roundedinput"><input require="true" label="logname_label" id="login_input" name="logname" type="text" value="<?= $_POST[logname] ?>" /></div></td>
                 </tr>
                 <tr>
                     <td class="even"><span id="password_label"><? echo $webui_password ?>*</span></td>
-                    <td class="even"><input require="true" label="password_label" id="login_input" type="password" name="logpassword" /></td>
+                    <td class="even"><div class="roundedinput"><input require="true" label="password_label" id="login_input" type="password" name="logpassword" /></div></td>
                 </tr>
                 <tr>
-                    <td class="odd"><a href="index.php?page=forgotpass"><? echo $webui_forgot_password ?></a></td>
-                    <td class="odd"><input id="login_bouton" type="submit" name="Submit" value="<? echo $webui_login ?>" /></td>
+                    <td class="odd"><a href="index.php?page=forgotpass"><? echo $webui_forgot_password ?></a></td>                
+                    <td class="odd">
+                      <div class="center">
+                        <button id="login_bouton" type="Submit" name="Submit" value="<? echo $webui_login ?>"><? echo $webui_login; ?></button>
+                      </div>
+                    </td>
                 </tr>
             </table>
         </form>
