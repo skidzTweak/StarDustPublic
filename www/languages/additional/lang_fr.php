d6adf29d9e625032715951e072c0c04034f49f4e
djphil <digitalconcepts@free.fr>
2012-09-12 02:54:08 +0300

    Update www/languages/additional/lang_fr.php
    Translate

--- www/languages/additional/lang_fr.php
+++ www/languages/additional/lang_fr.php
@@ -1,31 +1,30 @@
 <?php
-$webui_menu_item_getregion='Buy Land';
+$webui_menu_item_getregion='Acheter un Terrain'; // Buy Land
 
-$webui_menu_item_getcurrency='Buy G$';
-$webui_menu_item_getcurrencyhistory='History';
-$webui_menu_item_currencytransfer='Transfer';
-$webui_transactions_found='Transactions found';
+$webui_menu_item_getcurrency='Acheter G$';
+$webui_menu_item_getcurrencyhistory='Historique';
+$webui_menu_item_currencytransfer='Transfert';
+$webui_transactions_found='Transactions found'; 
 $webui_transactions_list_info='This is a list of your transactions.';
 
-$site_not_a_member='Not a member?';
-$site_sign_up_today='Créez votre compte GRATUIT sur notre site.';
+$site_not_a_member='N\'est pas un membre?';
+$site_sign_up_today='Créez Votre Compte GRATUIT sur Notre site.';
 
 $site_sign_up_benifit='
-<ul><li>Large wonderful world with friendly people</li>
-<li>Security is a top priority</li>
-<li>Choose your default avatar when you sign up</li>
-<li>Latest physics engine</li>
-<li>Over 100 Regions</li>
-<li>Currency compatible with lindens and us and euros</li>
-<li>No purchase fee</li>
-<li>No set up fee</li>
-<li>The worlds only virtual 3D dating environment</li>
-<li>You can keep you sl name and  identity</li>
-<li>Friendly personal owners</li>
+<ul><li>Grand monde merveilleux avec des gens sympathiques</li>
+<li>La sécurité est une priorité absolue</li>
+<li>Choisissez votre avatar par défaut lorsque vous vous inscrivez</li>
+<li>Dernier moteur physique</li>
+<li>Plus de 100 régions</li>
+<li>Compatible avec les devises de Lindens et US et €</li>
+<li>Pas de frais d\'achat</li>
+<li>Aucun frais d\'installation</li>
+<li>Les mondes virtuels en 3D que datation environnement</li>
+<li>Vous pouvez garder votre nom et l\'identité sl</li>
+<li>Propriétaires bienvenus personnels</li>
 <li>Kick ass administer and scripters</li>
-</ul>
-';
+</ul>';
 
-$site_terms_of_service_agree='I agree with the Terms of Service.';
-$ErrorMessagePurchaseAlreadyComplete = "This purchase has already been completed. If you have not seen the currency applied yet, please be patient, as it might be on hold.";
+$site_terms_of_service_agree='Je suis d\'accord avec les Conditions d\'utilisation.';
+$ErrorMessagePurchaseAlreadyComplete = "Cet achat a déjà été complété. Si vous n'avez pas vu la monnaie encore appliqué, s\'il vous plaît soyez patient, car cela pourrait être en attente.";
 ?>
